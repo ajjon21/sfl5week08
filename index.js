18cb8a2084d3206b37d2bf1b96f5efdc889a8a03
ajjon21 <74731427+ajjon21@users.noreply.github.com>
2026-03-31 11:03:56 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 80
+app.get("/" , (request,response)=> {
+    response.send("Hello from NODE JS Application")
+})
+app.listen(port, ()=>{console.log ("Server is up and running" + port)});
